42a8bda45b9e83887a7ca17607011a343d7b1ba0
Matthieu Patou <mat@matws.net>
2023-09-12 13:13:49 -0700

    Deal with flow failing in MacOS based Docker containers

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Flow is not working really in Docker containers running in a Mac M1 (at least)
+# Anyhow flow is more useful for the development than for running so let's make this optional
 set -e
-npm run flow
+if [ ! -e /proc/self/cgroup ]||[ grep -q -v docker /proc/self/cgroup ]; then
+  npm run flow
+fi
 npm run build
 npm run bundle
 npm run minify
